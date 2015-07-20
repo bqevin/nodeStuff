00f77d356765cb75e4216ad7682225d2545a2879
bqevin <bkevin001@yahoo.com>
2015-07-20 18:38:18 +0300

    Node Buster

--- routes/blog/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-
-router = express.Router();
-router.get( '/', function (req, res) {
-	res.send('this is blog folder');
-});
-
-module.exports = router;
--- routes/photography/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-
-router = express.Router();
-router.get( '/', function (req, res) {
-	res.send('this is photos folder');
-});
-
-module.exports = router;
